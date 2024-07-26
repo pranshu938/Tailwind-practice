479728211fd9d0dafbc809b0bbe82e298245a405
erpranshu2594 <pranshu.kumar@mtraction.com>
2024-07-26 12:05:13 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="box-border p-0 mr-0">
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ src/components/navbar.js
@@ -0,0 +1,22 @@
+const Navbar = () => {
+  return (
+    <div className="flex flex-row py-5">
+      {/* <div className="px-10">logo</div>
+      <ul className="flex flex-row list-none">
+        <li className="px-10">Data Science & AI</li>
+        <li className="px-10">B.Tech in CS & AI</li>
+        <li className="px-10">Newton Edge</li>
+        <li className="px-10">Refer & Earn</li>
+        <li className="px-10">More</li>
+        <li className="px-10">
+          <button>Signup</button>
+        </li>
+      </ul> */}
+      <div class="flex-auto">01</div>
+      <div class="flex-auto">02</div>
+      <div class="flex-auto">03</div>
+    </div>
+  );
+};
+
+export default Navbar;
